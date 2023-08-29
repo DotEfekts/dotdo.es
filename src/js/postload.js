6ee67010d7a09fd2f672ac611bd3553118c0d07e
Chelsea Pritchard <chelsea@dotefekts.net>
2023-08-29 22:25:53 +0800

    Move non-essential scripts/styles to post window load.

---
+++ src/js/postload.js
@@ -0,0 +1,29 @@
+window.addEventListener('load', function() {
+    loadSheet('/css/music.min.css');
+    loadSheet('/css/search.min.css');
+    loadSheet('/css/print.min.css');
+
+    loadScript('/js/search.min.js');
+    loadScript('/js/music.min.js');
+
+    loadSheet('/css/vendor/highlightjs-copy.min.css');
+    loadSheet('/css/highlight.min.css');
+
+    loadScript('/js/vendor/highlightjs-copy.min.js', () => loadHljsCopy());
+    loadScript('/js/vendor/highlight/highlight.min.js', () => loadHljs());
+});
+
+function loadSheet(url) {
+    let sheet = document.createElement("link");
+    sheet.rel = "stylesheet";
+    sheet.rel = url;
+    document.body.append(sheet);
+}
+
+function loadScript(url, callback) {
+    let script = document.createElement("script");
+    script.src = url;
+    script.async = true;
+    script.onload = callback;
+    document.body.append(script);
+}
